d80aab2b675669e16e0b9011ecff844f4ff1f5cd
kiyoung lee <ky27100@gmail.com>
2018-09-26 10:44:38 +0900

    Update FL-helper.js

--- manuscript/FL-helper.js
+++ manuscript/FL-helper.js
@@ -1,20 +1,5 @@
 "use strict";
 
-function reduce(reducer, arr, initial = undefined){
-	var acc = initial, i=0;
-	
-	if (!initial) {
-		acc = arr[0];
-		i=1;
-	}
-
-	for( i; i<arr.length; i++){
-		acc = reducer(acc, arr[i]);
-	}	
-
-	return acc;
-}
-
 var pipe = reverseArgs(compose);
 
 // curried list operators
