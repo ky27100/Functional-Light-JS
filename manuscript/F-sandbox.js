09825fb0c0e7a30c88409d4c33cef2ad00fae60e
kiyoung lee <ky27100@gmail.com>
2018-10-10 09:40:10 +0900

    Update F-sandbox.js

--- manuscript/F-sandbox.js
+++ manuscript/F-sandbox.js
@@ -1,1 +1,21 @@
 
+function curryK(fn, arity){
+	return (function looping(paras){
+		return function curried(arg){
+			var args = [...paras,arg];
+			if (args.length >= arity){ return fn(...args)}
+			else { return looping(args)}
+		}	
+	})([])
+
+}
+
+function curryK2(fn, arity){
+	var args = [];
+	return function currying(param){
+		args.push(param);
+		if (args.length >= arity) return fn(...args) 
+		else return currying		
+
+	}
+}
